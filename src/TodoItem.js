0274470139b96910bfd671ecb12ac131ffbc0873
Fernando Mejia Laguna <mejia190899@gmail.com>
2023-12-13 22:38:21 -0600

    Added dynamic Icons

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,23 +1,19 @@
+import { CompleteIcon } from "./CompleteIcon";
+import { DeleteIcon } from "./DeleteIcon";
 import "./TodoItem.css";
+
 function TodoItem({ text, completed, onComplete, onDelete }) {
   return (
     <li className="TodoItem">
-      <span
-        className={`Icon Icon-check 
-      ${completed && "Icon-check--active"}`}
-        onClick={onComplete}
-      >
-        V
-      </span>
+      <CompleteIcon completed={completed} onComplete={onComplete} />
+
       <p
         className={`TodoItem-p
         ${completed && "TodoItem-p--complete"}`}
       >
         {text}
       </p>
-      <span className="Icon Icon-delete" onClick={onDelete}>
-        X
-      </span>
+      <DeleteIcon onDelete={onDelete} />
     </li>
   );
 }
